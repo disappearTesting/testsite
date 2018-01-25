77c824b6e97ae8f2f877fe79da156c276ede8dd3
Makarov K <makarov@smartproject.ua>
2018-01-25 22:37:36 +0200

    new file:   test/pom/PomRegister.java

--- test/pom/PomLogin.java
+++ test/pom/PomLogin.java
@@ -12,6 +12,7 @@ public class PomLogin {
     WebDriver driver;
 
     By username = By.name("username");
+    By useremail = By.name("useremail");
     By password = By.name("password");
     By submitbutton = By.name("submitbutton");
 
@@ -42,6 +43,12 @@ public class PomLogin {
 
     public void loginSetNamePassword(String name, String pass) {
         driver.findElement(username).sendKeys(name);
+        driver.findElement(password).sendKeys(pass);
+        driver.findElement(submitbutton).submit();
+    }
+
+    public void loginSetEmailPassword(String email, String pass) {
+        driver.findElement(useremail).sendKeys(email);
         driver.findElement(password).sendKeys(pass);
         driver.findElement(submitbutton).submit();
     }
