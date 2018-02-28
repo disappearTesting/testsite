c3764d52fa4f84f80ecf649a9f588b6b6cd26629
Makarov_K <makarov@smartproject.ua>
2018-02-28 11:25:13 +0200

    deleted: loginPage folder

--- test/pom/DatetimeKendoUIPage.java
+++ test/pom/DatetimeKendoUIPage.java
@@ -8,17 +8,26 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DatetimeKendoUIPage {
 
     private WebDriver driver;
 
     private By inputDateTime = By.id("input-datetime");
-    private By inputDate = By.id("input-date");
-    private By inputTime = By.id("input-time");
+    private By iconDatetime = By.xpath("/html/body/div[1]/content/form/fieldset/div[1]/div/span[2]");
+    private By iconRemoveDatetime = By.xpath("/html/body/div[1]/content/form/fieldset/div[1]/div/span[1]");
+    private By thSwitchDatetime = By.xpath("/html/body/div[2]/div[3]/table/thead/tr[1]/th[2]");
+    private By thNextDatetime = By.xpath("/html/body/div[2]/div[3]/table/thead/tr[1]/th[3]");
+    private By thPreviousDatetime = By.xpath("/html/body/div[2]/div[3]/table/thead/tr[1]/th[1]");
+    private By thTodayDatetime = By.xpath("/html/body/div[2]/div[3]/table/tfoot/tr[1]/th");
 
-    private By datetimeDropdownMenu = By.xpath("/html/body/div[1]/content/form/fieldset/div[1]/div/span[2]");
-    private By datetDropdownMenu = By.xpath("/html/body/div[1]/content/form/fieldset/div[2]/div/span[2]");
-    private By timetDropdownMenu = By.xpath("/html/body/div[1]/content/form/fieldset/div[3]/div/span[2]");
+    private By inputDate = By.id("input-date");
+    private By iconDate = By.xpath("/html/body/div[1]/content/form/fieldset/div[2]/div/span[2]");
+
+    private By inputTime = By.id("input-time");
+    private By iconTime = By.xpath("/html/body/div[1]/content/form/fieldset/div[3]/div/span[2]");
 
     public DatetimeKendoUIPage(WebDriver driver) {
         this.driver = driver;
@@ -28,38 +37,19 @@ public class DatetimeKendoUIPage {
         return inputDateTime;
     }
 
-    public By getInputDate() {
-        return inputDate;
+    public By getIconDatetime() {
+        return iconDatetime;
     }
 
-    public By getInputTime() {
-        return inputTime;
-    }
+    public List<WebElement> getElementsDatetimeDropdownMenu() {
 
-    public By getDatetimeDropdownMenu() {
-        return datetimeDropdownMenu;
-    }
+        List<WebElement> elementsDatetimeDropdownMenu = new ArrayList();
+        elementsDatetimeDropdownMenu.add(driver.findElement(iconRemoveDatetime));
+        elementsDatetimeDropdownMenu.add(driver.findElement(thSwitchDatetime));
+        elementsDatetimeDropdownMenu.add(driver.findElement(thNextDatetime));
+        elementsDatetimeDropdownMenu.add(driver.findElement(thPreviousDatetime));
+        elementsDatetimeDropdownMenu.add(driver.findElement(thTodayDatetime));
 
-    public By getDatetDropdownMenu() {
-        return datetDropdownMenu;
-    }
-
-    public By getTimetDropdownMenu() {
-        return timetDropdownMenu;
-    }
-
-    public void setDateTime(String datetime) {
-        WebElement element = driver.findElement(inputDateTime);
-        element.sendKeys(datetime);
-    }
-
-    public void setDate(String date) {
-        WebElement element = driver.findElement(inputDate);
-        element.sendKeys(date);
-    }
-
-    public void setTime(String time) {
-        WebElement element = driver.findElement(inputTime);
-        element.sendKeys(time);
+        return elementsDatetimeDropdownMenu;
     }
 }
