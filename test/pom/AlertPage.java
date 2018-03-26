6d88fb84c2e41a80ba295a36469e5af7e75e5ad7
Makarov_K <makarov@smartproject.ua>
2018-03-26 09:35:49 +0300

    added: AlertPage pom-file

---
+++ test/pom/AlertPage.java
@@ -0,0 +1,39 @@
+/**
+ * @author hookie
+ * @version 1.0
+ */
+package pom;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AlertPage {
+
+    private WebDriver driver;
+
+    private By inputClickSimpleAlert = By.name("input_simple_alert");
+    private By inputClickConfirmAlert = By.name("input_confirm_alert");
+    private By inputClickPromptAlert = By.name("input_prompt_alert");
+
+    public AlertPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public boolean checkSimpleAlert() {
+        boolean result = false;
+        if(driver.findElement(inputClickSimpleAlert).isEnabled()) {
+            try {
+                driver.findElement(inputClickConfirmAlert).click();
+                new WebDriverWait(driver, 5).until(ExpectedConditions.alertIsPresent());
+                driver.switchTo().alert().accept();
+            } catch(NoAlertPresentException e) {
+
+            }
+        }
+        return result;
+    }
+}
---
+++ test/unitTests/Unit_AlertPage.java
@@ -0,0 +1,7 @@
+package unitTests;
+
+/**
+ * Created by Makarov_K on 26.03.2018.
+ */
+public class Unit_AlertPage {
+}
