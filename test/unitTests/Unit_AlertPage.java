a087d0edf2742fb776b4a4ce8be9baa29b311691
Makarov_K <makarov@smartproject.ua>
2018-03-26 09:54:57 +0300

    added: Unit_AlertPage file

--- test/unitTests/Unit_AlertPage.java
+++ test/unitTests/Unit_AlertPage.java
@@ -1,7 +1,19 @@
+/**
+ * @author hookie
+ * @version 1.0
+ */
 package unitTests;
 
-/**
- * Created by Makarov_K on 26.03.2018.
- */
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import pom.AlertPage;
+
 public class Unit_AlertPage {
+
 }
