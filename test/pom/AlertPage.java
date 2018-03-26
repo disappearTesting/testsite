5c931e4cd08ccae00af9f3db5974c2a9f9e7c6e0
Makarov K <makarov@smartproject.ua>
2018-03-26 22:18:39 +0300

    updated: files

--- test/pom/AlertPage.java
+++ test/pom/AlertPage.java
@@ -1,56 +1,3 @@
-<<<<<<< HEAD
-package pom;
-
-import org.openqa.selenium.*;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-public class AlertPage {
-
-    private WebDriver driver;
-
-    private By buttonClickSimpleAlert = By.name("input_simple_alert");
-    private By buttonClickConfirmAlert = By.name("input_confirm_alert");
-    private By buttonClickPromptAlert = By.name("input_prompt_alert");
-
-    public AlertPage(WebDriver driver) {
-        this.driver = driver;
-    }
-
-    public boolean checkSimpleAlert() {
-        boolean result = false;
-        // make WebElement object
-        if(driver.findElement(buttonClickSimpleAlert).isEnabled()) {
-            try {
-                driver.findElement(buttonClickSimpleAlert).click();
-                new WebDriverWait(driver,5).until(ExpectedConditions.alertIsPresent());
-                driver.switchTo().alert().accept();
-                result = true;
-            } catch(NoAlertPresentException e) {
-                System.out.println("Something went wrong!");
-            }
-        }
-        return result;
-    }
-
-    public boolean getConfirmAlert() {
-        boolean result = false;
-        if(driver.findElement(buttonClickConfirmAlert).isEnabled()) {
-            driver.findElement(buttonClickConfirmAlert).click();
-            result = true;
-        }
-        return result;
-    }
-
-    public boolean getPromptAlert() {
-        boolean result = false;
-        if(driver.findElement(buttonClickPromptAlert).isEnabled()) {
-
-        }
-        return result;
-    }
-}
-=======
 /**
  * @author hookie
  * @version 1.0
@@ -118,4 +65,3 @@ public class AlertPage {
         return  result;
     }
 }
->>>>>>> f12c9af833bd42d859b227d9e65048d2d29efda0
--- test/unitTests/Unit_AlertPage.java
+++ test/unitTests/Unit_AlertPage.java
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-package unitTests;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import static org.junit.Assert.assertTrue;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import pom.AlertPage;
-
-public class Unit_AlertPage {
-
-    private static final String URL_ALERT_PAGE = "http://testsite.local/rest/alertPage/index.php";
-
-    private WebDriver driver;
-    private AlertPage objAlert;
-
-    @Before
-    public void setUp() {
-        driver = new FirefoxDriver();
-        objAlert = new AlertPage(driver);
-        driver.get(URL_ALERT_PAGE);
-    }
-
-    @After
-    public void tearDown() {
-        //driver.quit();
-    }
-
-    @Test
-    public void test_CheckSimpleAlert() {
-        assertTrue("Something went wrong! checkSimpleAlert()", objAlert.checkSimpleAlert());
-    }
-}
-=======
 /**
  * @author hookie
  * @version 1.0
@@ -90,4 +52,3 @@ public class Unit_AlertPage {
         assertTrue(driver.getPageSource().contains("User wants to continue!"));
     }
 }
->>>>>>> f12c9af833bd42d859b227d9e65048d2d29efda0
