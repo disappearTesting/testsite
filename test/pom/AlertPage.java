0006e903eaf5e1706cd957e625b4e7bd216ba5fd
Makarov K <makarov@smartproject.ua>
2018-03-26 22:39:20 +0300

    updated: files

--- test/pom/AlertPage.java
+++ test/pom/AlertPage.java
@@ -8,9 +8,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 public class AlertPage {
 
     private WebDriver driver;
--- test/unitTests/Unit_AlertPage.java
+++ test/unitTests/Unit_AlertPage.java
@@ -34,7 +34,7 @@ public class Unit_AlertPage {
 
     @After
     public void tearDown() {
-        //driver.quit();
+        driver.quit();
     }
 
     @Test
