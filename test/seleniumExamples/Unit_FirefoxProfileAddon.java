ac6d74c6abfbfd1323aa3cce710183b4b9924b00
Makarov_K <makarov@smartproject.ua>
2018-04-03 11:10:53 +0300

    added: Unit_FirefoxProfileAddon.java file

---
+++ test/seleniumExamples/Unit_FirefoxProfileAddon.java
@@ -0,0 +1,19 @@
+/**
+ * @author hookie
+ * @version 1.0
+ */
+
+package seleniumExamples;
+
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+
+public class Unit_FirefoxProfileAddon {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+
+    }
+}
