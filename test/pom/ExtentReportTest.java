985cb70678742527a6367372bce8cb64830cbb30
Makarov_K <makarov@smartproject.ua>
2018-05-02 16:58:13 +0300

    modified: ExtentReportTest class

--- test/pom/ExtentReportTest.java
+++ test/pom/ExtentReportTest.java
@@ -3,6 +3,19 @@
  */
 package pom;
 
+import com.relevantcodes.extentreports.ExtentReports;
+
 public class ExtentReportTest {
+
+    // Create object of extent report and specify the report file path.
+    ExtentReports extent;
+
+    public ExtentReportTest(String path, String filename) {
+        this.extent = new ExtentReports(System.getProperty(path) + filename, false);
+    }
+
+    public ExtentReports getExtent() {
+        return extent;
+    }
 
 }
--- test/unitTests/Unit_ActionPage.java
+++ test/unitTests/Unit_ActionPage.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import pom.ActionPage;
+import pom.ExtentReportTest;
 import pom.TestRunException;
 
 import java.io.IOException;
