339a15e0bd47d43ed06eb9b7446d79adf6789ec1
Makarov K <makarov@smartproject.ua>
2018-05-24 23:36:14 +0300

    added: writeTheCookieFile(), readeTheCookieFile() methods

--- test/pom/GetCookies.java
+++ test/pom/GetCookies.java
@@ -3,36 +3,67 @@ package pom;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.Date;
+import java.util.StringTokenizer;
 
 public class GetCookies {
 
     private WebDriver driver;
+    private String cookieFileName;
 
-    public GetCookies(WebDriver driver) {
+    public GetCookies(WebDriver driver, String cookieFileName) {
         this.driver = driver;
+        this.cookieFileName = cookieFileName;
     }
 
     // logical method
-    public BufferedWriter getCookies(String fileName) throws IOException {
-        File file = new File(fileName);
+    private BufferedWriter writeTheCookieFile() throws IOException {
+        File file = new File(cookieFileName);
         BufferedWriter bufferedWriter = null;
         if (file.exists()) {
             file.delete();
         } else {
             file.createNewFile();
-            FileWriter writer = new FileWriter(file);
-            bufferedWriter = new BufferedWriter(writer);
+            FileWriter fileWriter = new FileWriter(file);
+            bufferedWriter = new BufferedWriter(fileWriter);
             for (Cookie cookie : driver.manage().getCookies()) {
                 bufferedWriter.write(cookie.getName() + ";" + cookie.getValue() + ";" + cookie.getDomain() + ";" + cookie.getPath() + ";" + cookie.getExpiry() + ";" + cookie.isSecure());
                 bufferedWriter.newLine();
             }
             bufferedWriter.close();
-            writer.close();
+            fileWriter.close();
         }
         return bufferedWriter;
+    }
+
+    // logical method
+    private BufferedReader readeTheCookieFile() throws IOException {
+        File file = new File(cookieFileName);
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String strLine = bufferedReader.readLine();
+        while(strLine != null){
+            StringTokenizer token = new StringTokenizer(strLine,";");
+            while(token.hasMoreTokens()){
+                String name = token.nextToken();
+                String value = token.nextToken();
+                String domain = token.nextToken();
+                String path = token.nextToken();
+                Date expiry = null;
+
+                String val;
+                if(!(val=token.nextToken()).equals("null"))
+                {
+                    expiry = new Date(val);
+                }
+                Boolean isSecure = new Boolean(token.nextToken()).
+                        booleanValue();
+                Cookie cookie = new Cookie(name,value,domain,path,expiry,isSecure);
+                System.out.println(cookie);
+                driver.manage().addCookie(cookie); // This will add the stored cookie to your current session
+            }
+        }
+        return bufferedReader;
     }
 }
