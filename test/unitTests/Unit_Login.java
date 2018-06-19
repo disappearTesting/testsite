f35654705797f15bbcef77a52eec22e85c8539e9
Makarov_K <makarov@smartproject.ua>
2018-06-19 08:50:35 +0300

    updated: files

--- test/unitTests/Unit_Login.java
+++ test/unitTests/Unit_Login.java
@@ -23,15 +23,8 @@ import static org.junit.Assert.assertTrue;
 public class Unit_Login {
 
     private static final String URL_LOGIN_PAGE = "http://testsite.local/rest/loginPage/login.php";
-<<<<<<< HEAD
-    //private static final String COOKIE_PATH = "C:\\Users\\hookie\\IdeaProjects\\testsite\\rest\\loginPage\\folderToCookiesFile\\";
-    private static final String COOKIE_PATH = "C:\\Users\\Makarov_K\\IdeaProjects\\testsite\\rest\\loginPage\\folderToCookiesFile\\";
-    //private static final String COOKIE_FILENAME = "LoginPage.Cookies.data";
-    private static final String COOKIE_FILENAME = "LightShot.Cookies.data";
-=======
     private static final String COOKIE_PATH = "C:\\Users\\hookie\\IdeaProjects\\testsite\\rest\\loginPage\\folderToCookiesFile\\";
     private static final String COOKIE_FILENAME = "LoginPage.Cookies.data";
->>>>>>> c882f6d5c87b0d5ff303345b9a226a76bef3064e
 
     private WebDriver driver;
     private Login objLogin;
@@ -64,22 +57,4 @@ public class Unit_Login {
         objCookies.getTheCookieFile();
         assertTrue(objCookies.checkExistingCookieFile(COOKIE_PATH, COOKIE_FILENAME));
     }
-<<<<<<< HEAD
-
-    @Test
-    public void test() throws IOException {
-        objCookies = new GetCookies(driver, COOKIE_PATH + COOKIE_FILENAME);
-        driver.get("https://prntscr.com/gallery.html");
-        objCookies.getTheCookieFile();
-    }
-
-    @Test
-    public void test_T() throws InterruptedException, IOException, ParseException {
-        driver.manage().deleteAllCookies();
-        objCookies.addCookies();
-        Thread.sleep(3000);
-        driver.get("https://prntscr.com/gallery.html");
-    }
-=======
->>>>>>> c882f6d5c87b0d5ff303345b9a226a76bef3064e
 }
