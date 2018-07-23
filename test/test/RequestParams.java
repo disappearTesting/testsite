5292a86572d5b76f70822f246edf3883c89a4122
Makarov K <makarov@smartproject.ua>
2018-07-23 22:38:37 +0300

    updated: setParams() method

--- test/test/RequestParams.java
+++ test/test/RequestParams.java
@@ -1,16 +1,24 @@
 package test;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
 public class RequestParams {
 
-    private Map<String, String> mapParams;
+    private String key;
+    private String value;
+
+    private RequestParams(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
 
     private Map<String, String> defineParams() {
-        mapParams = new HashMap<String, String>();
-        mapParams.put("user_id","3116818");
+        Map<String, String> mapParams = new HashMap<>();
+        mapParams.put(key,value);
         return mapParams;
     }
 
@@ -18,6 +26,23 @@ public class RequestParams {
         Map<String, String> map = defineParams();
         StringBuffer requestParams = new StringBuffer();
         Iterator<String> iterator = map.keySet().iterator();
-        requestParams.
+        while(iterator.hasNext()) {
+            try {
+                String key = iterator.next();
+                String value = map.get(key);
+                requestParams.append(URLEncoder.encode(key, "UTF-8"));
+                requestParams.append("=").append(
+                        URLEncoder.encode(value, "UTF-8"));
+                requestParams.append("&");
+                System.out.println(requestParams);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        return requestParams;
+    }
+
+    public static void main(String[] args) {
+        new RequestParams("user_id","3116818").setParams();
     }
 }
