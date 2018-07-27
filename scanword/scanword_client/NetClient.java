40c9db555be3b8e3a3b38151b310e5e166fe344e
Makarov_K <makarov@smartproject.ua>
2018-07-27 15:47:34 +0300

    added: scanword_tests folder, Unit_Scanword_SyncRequest class

--- scanword/scanword_client/NetClient.java
+++ scanword/scanword_client/NetClient.java
@@ -3,48 +3,94 @@ package scanword_client;
 import java.io.*;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
-import java.util.LinkedHashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class NetClient {
-    public static void main(String[] args) throws URISyntaxException, MalformedURLException {
 
-        URI uri = new URI("https", "scanword-stage.smapps.net", "/testsite_resources/sync/ios", null);
-        URL url = uri.toURL();
+    private HttpURLConnection httpURLConnection;
 
-        Map<String, String> params = new LinkedHashMap<>();
-        params.put("user_id", "3116963");
-        params.put("auth_key", "110d770ef9f1e6d81c2dce7c7b224f26");
-        params.put("vendor_id", "C9332507-6821-443E-BC09-0870A1BA1CB5");
-        params.put("advertising_id", "00000000-0000-0000-0000-000000000000");
-        params.put("advertising_track_enabled", "0");
-        params.put("vendor_key", "67cfffb331dae540bdded988c35c136b");
-        params.put("advertising_key", "575115c0a76bbc7cf76f331a9a933a0c");
-        params.put("device_info", "ewogICJkZXZpY2VfaGVpZ2h0IiA6ICI1NjgiLAogICJhcHBfdmVyc2lvbiIgOiAiMS43LjE2IiwKICAibGFzdF9zeW5jX2luZXRfdHlwZSIgOiAibm9fc3luY190eXBlIiwKICAiYXBwX2J1aWxkIiA6ICI2MiIsCiAgImFkdmVydGlzaW5nX3RyYWNrX2VuYWJsZWQiIDogIjAiLAogICJkZXZpY2VfcGxhdGZvcm0iIDogImlQaG9uZTYsMSIsCiAgImF1dG9sb2FkX25ld19pc3N1ZXNfb24iIDogIjAiLAogICJkZXZpY2Vfc3lzdGVtX3ZlcnNpb24iIDogIjEwLjIiLAogICJsb2NhbGl6YXRpb24iIDogInJ1IiwKICAiaXNzdWVzX2xvYWRlZCIgOiAiMCIsCiAgImRldmljZV93aWR0aCIgOiAiMzIwIgp9");
-        params.put("visit_id", "146bf9847a7913eb78ada4ed9f4f1b2c");
-        params.put("request_id", "48036c76ca6b47d45b0b89c98204a220");
-        params.put("updates", "ewogICJlOWM5ZmMyMTA2NmUzN2I1Y2YwYzY2ZmM4ZWJlNjI1OSIgOiB7CiAgICAidXVzIiA6IHsKICAgICAgInRtIiA6IDEKICAgIH0sCiAgICAiX3VvdCIgOiAxNTMyNDE5NTI2CiAgfSwKICAiNTU5OGQ0MzQ1N2E3YTFjNGY3Yjc3YTYxZmJhZWRmZDEiIDogewogICAgInV1cyIgOiB7CiAgICAgICJsYyIgOiAyLAogICAgICAibGYiIDogewogICAgICAgICJiYWNrZ3JvdW5kIiA6IDEKICAgICAgfQogICAgfSwKICAgICJfdW90IiA6IDE1MzI0MTk1MjUKICB9Cn0=");
-
-        String query = new QueryBuilder().setQuery(params).toString();
-        byte[] post = query.getBytes(StandardCharsets.UTF_8);
-
+    private HttpURLConnection getHttpURLConnection(URL url, String requestMethod) {
         try {
             url = new URL(url.toString());
-            HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setDoOutput(true);
+            httpURLConnection.setRequestMethod(requestMethod);
+            httpURLConnection.setRequestProperty("User-Agent", "Java scanword_client");
+            httpURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return httpURLConnection;
+    }
 
-            httpConnection.setDoOutput(true);
-            httpConnection.setRequestMethod("POST");
-            httpConnection.setRequestProperty("User-Agent", "Java scanword_client");
-            httpConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+    private StringBuffer buildQueryString(Map<String, String> params) {
+        StringBuffer requestParams = new StringBuffer();
+        Iterator<String> iterator = params.keySet().iterator();
+        while(iterator.hasNext()) {
+            try {
+                String key = iterator.next();
+                String value = params.get(key);
+                requestParams.append(URLEncoder.encode(key, "UTF-8"));
+                requestParams.append("=").append(URLEncoder.encode(value, "UTF-8"));
+                requestParams.append("&");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        return requestParams;
+    }
 
+    private byte[] getQueryByteArray(String query) {
+        return query.getBytes(StandardCharsets.UTF_8);
+    }
+
+    private void readContent() {
+        if(httpURLConnection != null) {
+            try {
+                BufferedReader input = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+                String inputLine;
+                StringBuilder content = new StringBuilder();
+                while((inputLine = input.readLine()) != null) {
+                    content.append(inputLine);
+                    content.append(System.lineSeparator());
+                }
+                System.out.println(content.toString());
+                input.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void doRequest(URL url, Map<String,String> requestParams, String method) {
+        try {
+            //
+            String query = buildQueryString(requestParams).toString();
+            byte[] post = getQueryByteArray(query);
+
+            //
+            HttpURLConnection httpConnection = getHttpURLConnection(url, method);
+
+            //
             DataOutputStream writer = new DataOutputStream(httpConnection.getOutputStream());
             writer.write(post);
             writer.flush();
 
-            new ReadContent().readContent(httpConnection);
-
+            //
+            readContent();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public int getResponseCode() throws IOException {
+        return httpURLConnection.getResponseCode();
+    }
+
+    public String getResponseMessage() throws IOException {
+        return httpURLConnection.getResponseMessage();
     }
 }
