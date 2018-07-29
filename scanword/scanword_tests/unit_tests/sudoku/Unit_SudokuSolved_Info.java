ac5229e28c74d99949d5fbcf7f088f672d98932f
Makarov K <makarov@smartproject.ua>
2018-07-29 23:17:38 +0300

    added: Unit_SudokuSolved_TestSuite class\

--- scanword/scanword_tests/unit_tests/sudoku/Unit_SudokuSolved_Info.java
+++ scanword/scanword_tests/unit_tests/sudoku/Unit_SudokuSolved_Info.java
@@ -32,10 +32,15 @@ public class Unit_SudokuSolved_Info {
     }
 
     @Test
-    public void test_SudokuSolved_Info() throws IOException, URISyntaxException {
+    public void test_SudokuSolved_Info() throws IOException {
         Map<String, String> paramsInfo = new LinkedHashMap<>();
         paramsInfo.put("user_id", "3116963");
         paramsInfo.put("auth_key", "110d770ef9f1e6d81c2dce7c7b224f26");
+        paramsInfo.put("friends", "");
+        paramsInfo.put("news_hash", "a3fd2c8b5ba5854eab1c7893ea0630cd");
+        paramsInfo.put("mask", "4");
+        paramsInfo.put("shop_hash", "97e37af106b975a610d4286ef8495755");
+        paramsInfo.put("rating_hash", "");
         paramsInfo.put("updates", "ewogICJmNzlhZTdiMmU1YzdmOGU3ZjkyY2ZjOTViMDFhMWFjYyIgOiB7CiAgICAidXVzIiA6IHsKICAgICAgInRzIiA6IDgzLAogICAgICAiaGNvIiA6IDQ5LAogICAgICAidG9uaCIgOiA0OSwKICAgICAgImhudSIgOiA0OQogICAgfSwKICAgICJ1c20iIDogewogICAgICAiczI4MjcwIiA6IHsKICAgICAgICAiaCIgOiAiICAuIC4uLi4gICAgIC4gIC4uLi4gIC4uIC4uLi4uLiAgLiAgLi4gLi4uLiAuLiAuLi4gLi4uIC4uLiAgLiAgLi4uIC4uLi4gICAgLi4gLi4uIiwKICAgICAgICAibSIgOiAwLAogICAgICAgICJuIiA6ICIiLAogICAgICAgICJhIiA6ICI4MjM5MTQ1Njc0NTY3ODIzOTE3OTEzNTY4MjQ5MTQ4MjU3MzYyNjU0Mzc5MTgzNzgxNjkyNDUxODcyNDM2NTk1Mzk2NzE0ODI2NDI1OTgxNzMiLAogICAgICAgICJmIiA6IDAKICAgICAgfQogICAgfSwKICAgICJfdW90IiA6IDE1MzI4ODg4MDIKICB9Cn0=");
 
         objClient.doRequest(url, paramsInfo, "POST");
--- scanword/scanword_tests/unit_tests/sudoku/Unit_SudokuSolved_TestSuite.java
+++ scanword/scanword_tests/unit_tests/sudoku/Unit_SudokuSolved_TestSuite.java
@@ -1,4 +1,30 @@
 package scanword_tests.unit_tests.sudoku;
 
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.RunWith;
+import org.junit.runner.notification.Failure;
+import org.junit.runners.Suite;
+
 public class Unit_SudokuSolved_TestSuite {
+
+    @RunWith(Suite.class)
+    @Suite.SuiteClasses({
+            Unit_SudokuSolved_SudokuListData.class,
+            Unit_SudokuSolved_Info.class
+    })
+
+    public class JunitTestSuite {
+
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(JunitTestSuite.class);
+
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+
+        System.out.println(result.wasSuccessful());
+    }
 }
